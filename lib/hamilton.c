e60bc5cc234f0f2c9ad662e74c4f166f5f92740b
Yesyouu <maysaputra0205@gmail.com>
2021-05-28 06:34:04 +0800

    update the hamilton function

--- lib/hamilton.c
+++ lib/hamilton.c
@@ -4,18 +4,17 @@
 #include <time.h>
 #define maxV 5 
 
-int h[maxV];
-
-int rando(int sum){
-    int n;
-    time_t j = time(NULL);
+// int rando(int sum){
+//     int n;
+//     time_t j = time(NULL);
     
-    srand(j);
-    n = (rand() % sum) + 1;
-    // else if();
-}
+//     srand(j);
+//     n = (rand() % sum) + 1;
+//     // else if();
+// }
 
-int ham(int matriks[maxV][maxV]){
+int ham(){
+    int matriks[maxV][maxV];
     int arrV[maxV];
     for(int i = 1; i<=maxV; i++){
         for(int j = 1; j<=maxV; j++){
@@ -23,19 +22,29 @@ int ham(int matriks[maxV][maxV]){
             else matriks[i][j] = 1;
         }
     }
-    time_t j = time(NULL);
-    srand(j);
-    arrV[1] = (rand() % maxV) + 1;
     int idx1, idx2;
-    for(int i = 2; i<=maxV; i++){
-        idx1 = arrV[i-1];
-        if(matriks[idx2][idx1] == 1){
-            
+    for(int i = 1; i<=maxV; i++){
+        for(int j = 1; j<=maxV; i++){
+            if(matriks[i][j] == 1 && matriks[j][i] == 1){
+                arrV[i] = i;
+            }
         }
+    }
+    srand(time(NULL));
+    // for(int i = 0; i < 5 ; i++){
+    //     arrV[i] = i + 1;
+    // }
+    for(int i = 0; i < 5 ; i++){
+        int randIndex = rand() % 5;
+        int temp = arrV[i];
+        arrV[i] = arrV[randIndex];
+        arrV[randIndex] = temp;
+    }   
+    for(int i = 0; i < 5 ; i++){
+        printf("%d  ", i);
     }
 }
 
 int main(){
-    int matriks[maxV][maxV];
-    ham(matriks);
+    ham();
 }
